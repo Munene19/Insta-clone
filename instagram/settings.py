f1215b009b5d2e9d41560e0ed0f9b037949f2be2
Mike Munene <you@example.com>
2020-11-23 07:13:15 +0300

    procfile moved

--- instagram/settings.py
+++ instagram/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_ROOT = [
-     os.path.join(BASE_DIR, "instagram/static/")
+STATIC_ROOT = [
+     os.path.join(BASE_DIR, "/static/")
 ]
 
 MEDIA_URL = '/media/'
