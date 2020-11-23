113179ba7c353020bae017d3e4e4318c27c3895e
Mike Munene <you@example.com>
2020-11-23 03:40:59 +0300

    Updated static root

--- instagram/settings.py
+++ instagram/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "instagram/static/")
+STATICFILES_ROOT = [
+     os.path.join(BASE_DIR, "instagram/staticfiles/")
 ]
 
 MEDIA_URL = '/media/'
